645409b2976e744b99d9d128bf42b4fd0f5a62ab
Ian <hellomotolegacy@gmail.com>
2017-10-06 10:59:37 -0400

    Added a stupid clear function and last name prompt
    xd

--- main.cpp
+++ main.cpp
@@ -3,15 +3,29 @@
 #include <string>
 
 //Global definitions
+std::string blankChar = ""; //Used for Prompt
 std::string Prompt = "Press ENTER to continue"; //FIXME - Find a better way :pp
 std::string firstName;
 std::string lastName;
+
+int clear() //Used for clearing the window
+{
+    std::cout << "\n\n";
+}
 
 int intro()
 {
     std::cout << "Sup fam give name xdddd\n";;
     getline (std::cin, firstName);
+    clear();
     std::cout << firstName << "? That's a stupid name xdddddddddddd\n";
+    std::cout << Prompt;
+    getline (std::cin, blankChar);
+    clear();
+    std::cout << "ok " << firstName << ", what about your last name?\nis it as stupid?\n";
+    getline (std::cin, lastName);
+    clear();
+    std::cout << "wow. " << firstName << " " << lastName << " is the dumbest name i've ever heard!";
 }
 
 int main()
